952628d91c5363dccb0e2b3d27ea279511894f24
RSurya99 <rafli.rfsp@gmail.com>
2022-03-29 10:39:49 +0700

    test: add BaseButton unit test and Home e2e test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Home Page', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'This is home page')
+    cy.contains('h1', 'Rafli Surya Pratama')
   })
 })
